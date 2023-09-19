fe156c3946e8efc58ff5e5cf35676a427f03464e
James Leahy <defuncart@gmail.com>
2023-09-19 20:16:37 +0200

    Basic folder selection

--- lib/features/list/ui/list_screen.dart
+++ lib/features/list/ui/list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:game_saves_backup/core/l10n/l10n_extension.dart';
@@ -16,10 +17,18 @@ class ListScreen extends ConsumerWidget {
       body: const _ListScreenContent(),
       floatingActionButton: FloatingActionButton.large(
         child: const Icon(Icons.add),
-        onPressed: () => ref.read(backupItemsProvider.notifier).add(const BackupItem(
-              path: 'path/to/folder',
-              folderName: 'MyFolder',
-            )),
+        onPressed: () async {
+          final directoryPath = await getDirectoryPath(
+            initialDirectory: 'Desktop/',
+            confirmButtonText: context.l10n.listScreenNewBackupItemSelectText,
+          );
+          if (directoryPath != null) {
+            ref.read(backupItemsProvider.notifier).add(BackupItem(
+                  path: directoryPath,
+                  folderName: directoryPath.split('/').last,
+                ));
+          }
+        },
       ),
     );
   }
