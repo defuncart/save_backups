6a2a61c9e82704a8a2293a08749e53ec5af4feea
James Leahy <defuncart@gmail.com>
2023-09-19 17:55:34 +0200

    Extract MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:game_saves_backup/core/ui/my_app.dart';
 
 void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('game_saves_backup'),
-        ),
-      ),
-    );
-  }
-}
