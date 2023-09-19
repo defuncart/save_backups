a7f07f7be8a3d4e2f1043a01d19939193343fcc6
James Leahy <defuncart@gmail.com>
2023-09-19 17:58:05 +0200

    Add theming

--- lib/core/ui/my_app.dart
+++ lib/core/ui/my_app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:game_saves_backup/core/theme/themes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      home: const Scaffold(
         body: Center(
           child: Text('game_saves_backup'),
         ),
