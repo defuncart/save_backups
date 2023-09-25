0277bdcfa66c5c3d369fe56610520800320b4d6e
James Leahy <defuncart@gmail.com>
2023-09-25 09:00:26 +0200

    Try getApplicationSupportDirectory

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:path_provider/path_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final dir = await getApplicationDocumentsDirectory();
+  final dir = await getApplicationSupportDirectory();
   Hive.defaultDirectory = dir.path;
   log('Hive.defaultDirectory: ${Hive.defaultDirectory}');
 
